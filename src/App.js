f7d942363b138d233f10e200d3ec60955e0cbaa2
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-11-20 20:44:09 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import HomePage from './pages/HomePage/HomePage';
 import Container from './components/Container/Container';
 import Phonebook from './pages/Phonebook';
+import Navigation from './pages/Navigation/Navigation';
 
 export default function App() {
   return (
     <Container>
+      <Navigation />
+      <HomePage />
       <Phonebook />
     </Container>
   );
